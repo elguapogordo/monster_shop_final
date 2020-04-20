9de41ef1bff22d823db194ff6108b3897a8ecea3
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-19 18:18:51 -0600

    add discount model validation and status method, add changes to database

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,11 @@ class Discount < ApplicationRecord
 
   validates_presence_of :percentage, :item_count
 
+  validates_inclusion_of :percentage, in: 1..100, message: 'The discount percentage must be between 1 and 100.'
+  validates :item_count, numericality: { greater_than: 0 }
+
+  def status
+    active? ? "active" : "inactive"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419200822) do
+ActiveRecord::Schema.define(version: 20200419234317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20200419200822) do
   create_table "discounts", force: :cascade do |t|
     t.integer "percentage"
     t.integer "item_count"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.bigint "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
