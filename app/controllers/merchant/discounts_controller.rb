66633ff20be0cbcf7f0db904def5b4689bf38e68
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-19 20:47:28 -0600

    add discounts controller

---
+++ app/controllers/merchant/discounts_controller.rb
@@ -0,0 +1,27 @@
+class Merchant::DiscountsController < Merchant::BaseController
+
+  def index
+    @merchant = current_user.merchant
+  end
+
+  def new
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to "/merchant/discounts"
+    else
+      generate_flash(discount)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percentage, :item_count)
+  end
+
+end
