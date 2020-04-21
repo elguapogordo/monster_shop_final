d5b2d09733ccdedec751254d910c323b1450311d
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-21 15:07:12 -0600

    add edit, discount to controller, updated_at to discount_params

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -22,10 +22,24 @@ class Merchant::DiscountsController < Merchant::BaseController
     end
   end
 
+  def edit
+    @discount = Discount.find(params[:id])
+  end
+
+  def update
+    discount = Discount.find(params[:id])
+    if discount.update(discount_params)
+      redirect_to "/merchant/discounts/#{discount.id}"
+    else
+      flash[:error] = discount.errors.full_messages.to_sentence
+      redirect_to "/merchant/discounts/#{discount.id}/edit"
+    end
+  end
+
   private
 
   def discount_params
-    params.permit(:percentage, :item_count)
+    params.permit(:percentage, :item_count, :updated_at)
   end
 
 end
