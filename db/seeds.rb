700c89e6fa5da0961d952397acae0e33f604347e
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-22 07:22:26 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,10 @@ user1 = User.create(name: 'Jesse Gietzen', address: '461 Ammo Dr', city: 'Boomto
 item1 = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
 item2 = megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 item3 = brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-item4 = mike.items.create!(name: 'Yosemite Sam', description: "Say yer prayers, rabbit!", price: 65, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
-item5 = mike.items.create!(name: 'Tom Selleck', description: "I'm not Burt Reynolds!", price: 35, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
-item6 = mike.items.create!(name: 'Wilford Brimley', description: "Diabeeetus!", price: 55, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
-item7 = mike.items.create!(name: 'Hulkster', description: "Brother!", price: 45, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+item4 = mike.items.create!(name: 'Yosemite Sam', description: "Say yer prayers, rabbit!", price: 65, image: 'https://pbs.twimg.com/profile_images/1071231200222679045/PDrkRGnY_400x400.jpg', active: true, inventory: 200 )
+item5 = mike.items.create!(name: 'Tom Selleck', description: "I'm not Burt Reynolds!", price: 35, image: 'https://lh6.googleusercontent.com/-sUSFs7-BAZU/AAAAAAAAAAI/AAAAAAAAADs/8O9-7oGyus4/photo.jpg', active: true, inventory: 200 )
+item6 = mike.items.create!(name: 'Wilford Brimley', description: "Diabeeetus!", price: 55, image: 'https://pbs.twimg.com/profile_images/3186541515/b3300a9dbe6e8454f6719dd236c23c2b.jpeg', active: true, inventory: 200 )
+item7 = mike.items.create!(name: 'Hulkster', description: "Brother!", price: 45, image: 'https://a.thumbs.redditmedia.com/u3FWi5KCbh0mrFQyyEXas9QIVGrl8QqTYFOcMS9kQ84.png', active: true, inventory: 200 )
 
 #discounts
 discount1 = mike.discounts.create(percentage: 5, item_count: 5, active: true)
