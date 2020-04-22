a20b7f09732ecc3d6ee2f2800bec074c264fdbba
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-22 05:23:29 -0600

    add tests, discount methods in cart model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Cart do
       @cart = Cart.new({
         @ogre.id.to_s => 1,
         @giant.id.to_s => 2
-        })
+      })
     end
 
     it '.contents' do
@@ -62,6 +62,44 @@ RSpec.describe Cart do
       @cart.less_item(@giant.id.to_s)
 
       expect(@cart.count_of(@giant.id)).to eq(1)
+    end
+  end
+
+  describe "instance methods for added bulk discounts" do
+    before :each do
+      @mike = Merchant.create(name: "Mike\'s Mustache Emporium", address: '127 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @megan = Merchant.create(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @item1 = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      @item2 = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @item4 = @mike.items.create!(name: 'Yosemite Sam', description: "Say yer prayers, rabbit!", price: 65, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+      @item5 = @mike.items.create!(name: 'Tom Selleck', description: "I'm not Burt Reynolds!", price: 35, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+      @item6 = @mike.items.create!(name: 'Wilford Brimley', description: "Diabeeetus!", price: 55, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+      @item7 = @mike.items.create!(name: 'Hulkster', description: "Brother!", price: 45, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 200 )
+      @discount1 = @mike.discounts.create(percentage: 5, item_count: 5, active: true)
+      @discount2 = @mike.discounts.create(percentage: 7, item_count: 10, active: true)
+      @discount3 = @megan.discounts.create(percentage: 10, item_count: 5, active: true)
+      @discount4 = @mike.discounts.create(percentage: 25, item_count: 1, active: false)
+      @cart = Cart.new({
+        @item1.id.to_s => 5,
+        @item2.id.to_s => 2,
+        @item5.id.to_s => 3,
+        @item6.id.to_s => 14,
+        @item7.id.to_s => 7,
+      })
+    end
+
+    it "top_discount(item_id)" do
+      expect(@cart.top_discount(@item1.id)).to eq(10)
+      expect(@cart.top_discount(@item2.id)).to eq(nil)
+      expect(@cart.top_discount(@item5.id)).to eq(nil)
+      expect(@cart.top_discount(@item6.id)).to eq(7)
+      expect(@cart.top_discount(@item7.id)).to eq(5)
+    end
+
+    it "discount_factor(item_id)" do
+      expect(@cart.discount_factor(@item1.id)).to eq(0.90)
+      expect(@cart.discount_factor(@item6.id)).to eq(0.93)
+      expect(@cart.discount_factor(@item7.id)).to eq(0.95)
     end
   end
 end
