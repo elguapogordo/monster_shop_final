b823e4e3a41dc7d73c53c0d9300f89a637b7ace6
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-21 20:48:44 -0600

    add destroy action to controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -36,6 +36,11 @@ class Merchant::DiscountsController < Merchant::BaseController
     end
   end
 
+  def destroy
+    Discount.destroy(params[:id])
+    redirect_to "/merchant/discounts"
+  end
+
   private
 
   def discount_params
